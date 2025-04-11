65cdc2964c876708dbc546042e448932d5dc8c81
Vedant-011 <vedant.me1@gmail.com>
2025-04-11 13:18:26 +0530

    code

---
+++ basics/functions.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// A function that takes no parameters and returns nothing
+func sayHello() {
+	fmt.Println("konnichiwa!")
+}
+
+// A function that takes parameters
+func add(a int, b int) {
+	fmt.Println("Sum:", a+b)
+}
+
+// A function that returns a value
+func multiply(a int, b int) int {
+	return a * b
+}
+
+// A function that returns multiple values
+func divideAndRemainder(a int, b int) (int, int) {
+	quotient := a / b
+	remainder := a % b
+	return quotient, remainder
+}
+
+func main() {
+	sayHello()
+
+	add(3, 4)
+
+	result := multiply(5, 6)
+	fmt.Println("Product:", result)
+
+	quotient, remainder := divideAndRemainder(10, 3)
+	fmt.Println("Quotient:", quotient)
+	fmt.Println("Remainder:", remainder)
+}
