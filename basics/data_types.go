38cddd672d4db5420d732b6b84505b079bace4d3
Vedant-011 <vedant.me1@gmail.com>
2025-06-15 23:52:41 +0530

    code

--- basics/data_types.go
+++ basics/data_types.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 )
 
 func main() {
@@ -20,6 +21,14 @@ func main() {
 	var g uint16 = 65535 // 0 to 65535
 	var h uint = 100     // Size depends on systen
 
+	// 2. Floating point (decimals)
+	var i float32 = 3.14              // Single precision
+	var j float64 = 3.141592653589793 // Double precision (more accurate)
+
+	// 3. Complex numbers  (for math/science applications)
+	var k complex64 = complex(1, 2) // 1+2i
+	var l complex128 = cmplx.Sqrt(-5 + 12i)
+
 	// Print everything to see the values
 	fmt.Println("INTEGERS:")
 	fmt.Println("int8:", a)
@@ -30,4 +39,12 @@ func main() {
 	fmt.Println("uint8:", f)
 	fmt.Println("uint16:", g)
 	fmt.Println("uint:", h)
+
+	fmt.Println("\nFLOATING POINT:")
+	fmt.Println("float32:", i)
+	fmt.Println("float64:", j)
+
+	fmt.Println("\nCOMPLEX NUMBERS:")
+	fmt.Println("complex64:", k)
+	fmt.Println("complex128:", l)
 }
